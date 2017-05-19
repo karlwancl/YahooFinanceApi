ee404f87c71e02ea3357ecc9f00fb42891dbc526
Karl Wan <salmonthinlion@gmail.com>
2017-05-19 20:03:43 +0800

    Fixes #2, use new Yahoo Finance URL

--- YahooFinanceApi/Yahoo - Historical.cs
+++ YahooFinanceApi/Yahoo - Historical.cs
@@ -5,27 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using Flurl.Http.Configuration;
 
 namespace YahooFinanceApi
 {
     public static partial class Yahoo
     {
-        private const string YahooFinanceUrl = "http://ichart.finance.yahoo.com/table.csv";
+        private const string QueryUrl = "https://query1.finance.yahoo.com/v7/finance/download";
+        private const string CookieUrl = "https://finance.yahoo.com";
+        private const string CrumbUrl = "https://query1.finance.yahoo.com/v1/test/getcrumb";
 
-        private const string SymbolTag = "s";
+        private const string Period1Tag = "period1";
+        private const string Period2Tag = "period2";
+        private const string IntervalTag = "interval";
+        private const string EventsTag = "events";
+        private const string CrumbTag = "crumb";
 
-        private const string FromMonthTag = "a";
-        private const string FromDayTag = "b";
-        private const string FromYearTag = "c";
-
-        private const string ToMonthTag = "d";
-        private const string ToDayTag = "e";
-        private const string ToYearTag = "f";
-
-        private const string TypeTag = "g";
         private static readonly IDictionary<Period, string> PeriodMap = new Dictionary<Period, string>
         {
             {Period.Daily, "d"},
@@ -33,69 +33,92 @@ namespace YahooFinanceApi
             {Period.Monthly, "m"}
         };
 
-        private const string DividendValue = "v";
-
-        private const string IgnoreTag = "ignore";
-        private const string CsvExtValue = ".csv";
+        private const string HistoryValue = "history";
+        private const string DividendValue = "div";
 
         public static async Task<IList<Candle>> GetHistoricalAsync(string symbol, DateTime? startTime = default(DateTime?), DateTime? endTime = default(DateTime?), Period period = Period.Daily, bool ascending = false, CancellationToken token = default(CancellationToken))
         {
             var candles = new List<Candle>();
-            using (var stream = await GetResponseStreamAsync(symbol, startTime, endTime, PeriodMap[period], token).ConfigureAwait(false))
+            using (var stream = await GetResponseStreamAsync(symbol, startTime, endTime, period, HistoryValue, token).ConfigureAwait(false))
             using (var sr = new StreamReader(stream))
             using (var csvReader = new CsvReader(sr))
             {
-                while (csvReader.Read())
-                {
-                    string[] row = csvReader.CurrentRecord;
-                    candles.Add(new Candle(
-                        Convert.ToDateTime(row[0]),
-                        Convert.ToDecimal(row[1]),
-                        Convert.ToDecimal(row[2]),
-                        Convert.ToDecimal(row[3]),
-                        Convert.ToDecimal(row[4]),
-                        Convert.ToInt64(row[5]),
-                        Convert.ToDecimal(row[6])
-                        ));
-                }
+               while (csvReader.Read())
+               {
+                   string[] row = csvReader.CurrentRecord;
+                   candles.Add(new Candle(
+                       Convert.ToDateTime(row[0]),
+                       Convert.ToDecimal(row[1]),
+                       Convert.ToDecimal(row[2]),
+                       Convert.ToDecimal(row[3]),
+                       Convert.ToDecimal(row[4]),
+                       Convert.ToInt64(row[6]),
+                       Convert.ToDecimal(row[5])
+                       ));
+               }
 
-                return ascending ? candles.OrderBy(c => c.DateTime).ToList() : candles.OrderByDescending(c => c.DateTime).ToList();
+               return ascending ? candles.OrderBy(c => c.DateTime).ToList() : candles.OrderByDescending(c => c.DateTime).ToList();
             }
         }
 
         public static async Task<IList<DividendTick>> GetHistoricalDividendsAsync(string symbol, DateTime? startTime = default(DateTime?), DateTime? endTime = default(DateTime?), bool ascending = false, CancellationToken token = default(CancellationToken))
         {
             var dividends = new List<DividendTick>();
-            using (var stream = await GetResponseStreamAsync(symbol, startTime, endTime, DividendValue, token).ConfigureAwait(false))
+            using (var stream = await GetResponseStreamAsync(symbol, startTime, endTime, Period.Daily, DividendValue, token).ConfigureAwait(false))
             using (var sr = new StreamReader(stream))
-                using (var csvReader = new CsvReader(sr))
+            using (var csvReader = new CsvReader(sr))
             {
-                while (csvReader.Read())
-                {
-                    string[] row = csvReader.CurrentRecord;
-                    dividends.Add(new DividendTick(
-                        Convert.ToDateTime(row[0]),
-                        Convert.ToDecimal(row[1])
-                        ));
-                }
+               while (csvReader.Read())
+               {
+                   string[] row = csvReader.CurrentRecord;
+                   dividends.Add(new DividendTick(
+                       Convert.ToDateTime(row[0]),
+                       Convert.ToDecimal(row[1])
+                       ));
+               }
 
-                return ascending ? dividends.OrderBy(c => c.DateTime).ToList() : dividends.OrderByDescending(c => c.DateTime).ToList();
+               return ascending ? dividends.OrderBy(c => c.DateTime).ToList() : dividends.OrderByDescending(c => c.DateTime).ToList();
             }
         }
 
-        private static async Task<Stream> GetResponseStreamAsync(string symbol, DateTime? startTime, DateTime? endTime, string type, CancellationToken token)
-            => await YahooFinanceUrl
-                .SetQueryParam(SymbolTag, symbol)
-                .SetQueryParam(FromMonthTag, startTime?.Month)
-                .SetQueryParam(FromDayTag, startTime?.Day)
-                .SetQueryParam(FromYearTag, startTime?.Year)
-                .SetQueryParam(ToMonthTag, endTime?.Month)
-                .SetQueryParam(ToDayTag, endTime?.Day)
-                .SetQueryParam(ToYearTag, endTime?.Year)
-                .SetQueryParam(TypeTag, type)
-                .SetQueryParam(IgnoreTag, CsvExtValue)
+        private static async Task<Stream> GetResponseStreamAsync(string symbol, DateTime? startTime, DateTime? endTime, Period period, string events, CancellationToken token)
+        {
+            using (var client = new FlurlClient().EnableCookies())
+            {
+                var crumb = await GetCrumbAsync(client, token).ConfigureAwait(false);
+                if (string.IsNullOrEmpty(crumb))
+                    throw new ArgumentNullException("Crumb is empty!");
+
+                var url = QueryUrl
+                    .AppendPathSegment(symbol)
+                    .SetQueryParam(Period1Tag, (startTime ?? new DateTime(1970, 1, 1)).ToUnixTimestamp())
+                    .SetQueryParam(Period2Tag, (endTime ?? DateTime.Now).ToUnixTimestamp())
+                    .SetQueryParam(IntervalTag, $"1{PeriodMap[period]}")
+                    .SetQueryParam(EventsTag, events)
+                    .SetQueryParam(CrumbTag, crumb);
+
+                return await client
+                    .WithUrl(url)
+                    .GetAsync(token)
+                    .ReceiveStream()
+                    .ConfigureAwait(false);
+            }
+        }
+
+        private static async Task<string> GetCrumbAsync(IFlurlClient client, CancellationToken token)
+        {
+            var cookieResponse = await client
+                .WithUrl(CookieUrl)
                 .GetAsync(token)
-                .ReceiveStream()
                 .ConfigureAwait(false);
+
+            cookieResponse.EnsureSuccessStatusCode();
+
+            return await client
+                .WithUrl(CrumbUrl)
+                .GetAsync(token)
+                .ReceiveString()
+                .ConfigureAwait(false);
+        }
     }
 }
