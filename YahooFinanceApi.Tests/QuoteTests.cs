0e6ad4bedf6a11767f1f5de69f5fd81f639f8d37
DavidS <david@tan.com>
2017-11-15 12:16:25 +0800

    properties added to Yahoo.QueryAsync()

--- YahooFinanceApi.Tests/QuoteTests.cs
+++ YahooFinanceApi.Tests/QuoteTests.cs
@@ -25,33 +25,11 @@ namespace YahooFinanceApi.Tests
         }
 
         [Fact]
-        public async Task TestYahooQuery()
-        {
-            IDictionary<string, IDictionary<string, dynamic>> securities =
-                await Yahoo
-                    .Symbols("C", "AAPL")
-                    .Fields("regularMarketPrice", "bid", "ask", "longName", "tradeable")
-                    .QueryAsync();
-
-            Assert.Equal(2, securities.Count());
-
-            dynamic bid = securities["C"]["bid"];
-
-            dynamic x = 2 * bid; // bid must be a numeric type
-
-            double ask = securities["C"]["ask"]; // ask must be double
-
-            Assert.True(securities["C"]["tradeable"]); // inferred type
-
-            Assert.Equal("Apple Inc.", securities["AAPL"]["longName"]); // inferred type
-        }
-
-        [Fact]
         public async Task TestYahooQueryArguments()
         {
             // no symbols
             await Assert.ThrowsAsync<ArgumentException>(async () => await Yahoo.Symbols().QueryAsync());
-            
+
             // symbol not found
             await Assert.ThrowsAsync<Flurl.Http.FlurlHttpException>(async () => await Yahoo.Symbols("invalidsymbol").QueryAsync());
 
@@ -60,8 +38,83 @@ namespace YahooFinanceApi.Tests
 
             // when no fields are specified, some default fields are returned
             await Yahoo.Symbols("C").QueryAsync();
-
         }
+
+        [Fact]
+        public async Task TestYahooQuery()
+        {
+            var securities = await Yahoo
+                    .Symbols("C", "AAPL")
+
+                    // can use strings and/or Field enum.
+                    .Fields("bid", "ask", "tradeable", "longName")
+                    .Fields(Field.regularMarketPrice, Field.currency)
+
+                    .QueryAsync();
+
+
+            Assert.Equal(2, securities.Count());
+
+
+            double bid1 = securities["C"]["bid"]; // dynamic
+
+            double bid2 = securities["C"][Field.bid]; // dynamic
+
+            double bid3 = securities["C"].bid; // double
+
+
+            Assert.True(securities["C"]["tradeable"]); // inferred type
+
+            Assert.Equal("Apple Inc.", securities["AAPL"]["longName"]); // inferred type
+        }
+
+        [Fact]
+        public async Task TestYahooQueryNotRequested()
+        {
+            var securities = await Yahoo.Symbols("AAPL").Fields(Field.symbol).QueryAsync();
+
+            var security = securities.First().Value;
+
+            // This field was requested and therefore is available.
+            Assert.Equal("AAPL", security.symbol);
+
+            // This field was not requested and therefore is not available.
+            Assert.Throws<KeyNotFoundException>(() => security.twoHundredDayAverageChange);
+
+            // Some fields are available although not requested.
+        }
+
+        [Fact]
+        public async Task MakeEnumList()
+        {
+            var securities = await Yahoo.Symbols("C").QueryAsync();
+            var fields = securities.First().Value.Fields;
+
+            Write("Paste into Field enum:" + Environment.NewLine);
+
+            Write("// This list was generated automatically. These names have been defined by Yahoo.");
+
+            Write(String.Join("," + Environment.NewLine, fields.Select(f => f.Key)));
+
+            Write(Environment.NewLine + ".");
+        }
+
+        [Fact]
+        public async Task MakePropertyList()
+        {
+            var securities = await Yahoo.Symbols("C").QueryAsync();
+            var fields = securities.First().Value.Fields;
+
+            Write(String.Format("{0}{0}Paste into class Security:{0}", Environment.NewLine));
+
+            Write("// This list was generated automatically. These names and types have defined by Yahoo.");
+
+            foreach (var field in fields)
+                Write($"public {field.Value.GetType().Name} {field.Key} => Fields[\"{field.Key}\"];");
+
+            Write(Environment.NewLine + ".");
+        }
+
 
     }
 }
--- YahooFinanceApi/Yahoo - Quote.cs
+++ YahooFinanceApi/Yahoo - Quote.cs
@@ -1,5 +1,4 @@
-﻿using CsvHelper;
-using Flurl;
+﻿using Flurl;
 using Flurl.Http;
 using System;
 using System.Collections.Generic;
@@ -13,16 +12,18 @@ namespace YahooFinanceApi
 {
     public partial class Yahoo
     {
-        private IList<string> symbols, fields;
+        private List<string> symbols;
+        private readonly HashSet<string> fields = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
         private Yahoo() { }
 
+        // static!
         public static Yahoo Symbols(params string[] symbols)
         {
             if (symbols == null || symbols.Length == 0 || symbols.Any(x => x == null))
                 throw new ArgumentException(nameof(symbols));
 
-            return new Yahoo { symbols = symbols };
+            return new Yahoo { symbols = new List<string>(symbols) };
         }
 
         public Yahoo Fields(params string[] fields)
@@ -30,21 +31,30 @@ namespace YahooFinanceApi
             if (fields == null || fields.Length == 0 || fields.Any(x => x == null))
                 throw new ArgumentException(nameof(fields));
 
-            this.fields = fields;
+            this.fields.UnionWith(fields);
 
             return this;
         }
 
-        public async Task<IDictionary<string, IDictionary<string, dynamic>>> 
-            QueryAsync(CancellationToken token = default(CancellationToken))
+        public Yahoo Fields(params Field[] fields)
+        {
+            if (fields == null || fields.Length == 0)
+                throw new ArgumentException(nameof(fields));
+
+            this.fields.UnionWith(fields.Select(f => f.ToString()));
+
+            return this;
+        }
+
+        public async Task<Dictionary<string, Security>> QueryAsync(CancellationToken token = default(CancellationToken))
         {
             if (!symbols.Any())
-                throw new ArgumentException("No symbols specified.");
+                throw new ArgumentException("No symbols indicated.");
 
             var url = "https://query1.finance.yahoo.com/v7/finance/quote"
                 .SetQueryParam("symbols", string.Join(",", symbols));
 
-            if (fields != null && fields.Any())
+            if (fields.Any())
                 url = url.SetQueryParam("fields", string.Join(",", fields));
 
             Debug.WriteLine(url);
@@ -58,16 +68,96 @@ namespace YahooFinanceApi
 
             var error = quoteResponse.error;
             if (error != null)
-                throw new InvalidDataException($"Yahoo.GetJsonAsync() error: {error}");
+                throw new InvalidDataException($"QueryAsync error: {error}");
 
-            var dictionary = new Dictionary<string, IDictionary<string, dynamic>>();
+            if (quoteResponse.result.Count != symbols.Count)
+                throw new InvalidDataException($"Received {quoteResponse.result.Count}/{symbols.Count} symbols.");
 
-            foreach (var security in quoteResponse.result)
-                dictionary.Add(security.symbol, security);
+            var securities = new Dictionary<string, Security>();
 
-            return dictionary;
+            // Note that the returned symbol (result) may be different from the requested symbol (key).
+            for (var i = 0; i < symbols.Count; i++)
+                securities.Add(symbols[i], new Security(quoteResponse.result[i]));
+
+            return securities;
         }
     }
 
+    public class Security
+    {
+        public IDictionary<string, dynamic> Fields { get; internal set; }
 
+        // ctor
+        internal Security(IDictionary<string, dynamic> fields) => Fields = fields;
+
+        public dynamic this[string fieldName] => Fields[fieldName];
+        public dynamic this[Field field] => Fields[field.ToString()];
+
+        // This list was generated automatically. These names and types have defined by Yahoo.
+        public String language => Fields["language"];
+        public String quoteType => Fields["quoteType"];
+        public String quoteSourceName => Fields["quoteSourceName"];
+        public String currency => Fields["currency"];
+        public Boolean tradeable => Fields["tradeable"];
+        public Double postMarketPrice => Fields["postMarketPrice"];
+        public Double postMarketChange => Fields["postMarketChange"];
+        public Double regularMarketChangePercent => Fields["regularMarketChangePercent"];
+        public String financialCurrency => Fields["financialCurrency"];
+        public Int64 averageDailyVolume3Month => Fields["averageDailyVolume3Month"];
+        public Int64 averageDailyVolume10Day => Fields["averageDailyVolume10Day"];
+        public Double fiftyTwoWeekLowChange => Fields["fiftyTwoWeekLowChange"];
+        public Double fiftyTwoWeekLowChangePercent => Fields["fiftyTwoWeekLowChangePercent"];
+        public Double postMarketChangePercent => Fields["postMarketChangePercent"];
+        public Int64 postMarketTime => Fields["postMarketTime"];
+        public Int64 marketCap => Fields["marketCap"];
+        public Double forwardPE => Fields["forwardPE"];
+        public Double priceToBook => Fields["priceToBook"];
+        public Int64 sourceInterval => Fields["sourceInterval"];
+        public Double fiftyTwoWeekHighChange => Fields["fiftyTwoWeekHighChange"];
+        public Double fiftyTwoWeekHighChangePercent => Fields["fiftyTwoWeekHighChangePercent"];
+        public Double fiftyTwoWeekLow => Fields["fiftyTwoWeekLow"];
+        public Double fiftyTwoWeekHigh => Fields["fiftyTwoWeekHigh"];
+        public Int64 dividendDate => Fields["dividendDate"];
+        public Double bookValue => Fields["bookValue"];
+        public Double fiftyDayAverage => Fields["fiftyDayAverage"];
+        public Double fiftyDayAverageChange => Fields["fiftyDayAverageChange"];
+        public Double fiftyDayAverageChangePercent => Fields["fiftyDayAverageChangePercent"];
+        public Double twoHundredDayAverage => Fields["twoHundredDayAverage"];
+        public Double twoHundredDayAverageChange => Fields["twoHundredDayAverageChange"];
+        public Double twoHundredDayAverageChangePercent => Fields["twoHundredDayAverageChangePercent"];
+        public Int64 earningsTimestamp => Fields["earningsTimestamp"];
+        public Int64 earningsTimestampStart => Fields["earningsTimestampStart"];
+        public Int64 earningsTimestampEnd => Fields["earningsTimestampEnd"];
+        public Double trailingAnnualDividendRate => Fields["trailingAnnualDividendRate"];
+        public Double trailingPE => Fields["trailingPE"];
+        public Double trailingAnnualDividendYield => Fields["trailingAnnualDividendYield"];
+        public Double epsTrailingTwelveMonths => Fields["epsTrailingTwelveMonths"];
+        public Double epsForward => Fields["epsForward"];
+        public Int64 sharesOutstanding => Fields["sharesOutstanding"];
+        public String exchangeTimezoneShortName => Fields["exchangeTimezoneShortName"];
+        public Int64 gmtOffSetMilliseconds => Fields["gmtOffSetMilliseconds"];
+        public Int64 priceHint => Fields["priceHint"];
+        public Double regularMarketPreviousClose => Fields["regularMarketPreviousClose"];
+        public Double bid => Fields["bid"];
+        public Double ask => Fields["ask"];
+        public Int64 bidSize => Fields["bidSize"];
+        public Int64 askSize => Fields["askSize"];
+        public String messageBoardId => Fields["messageBoardId"];
+        public String fullExchangeName => Fields["fullExchangeName"];
+        public String longName => Fields["longName"];
+        public Double regularMarketPrice => Fields["regularMarketPrice"];
+        public Int64 regularMarketTime => Fields["regularMarketTime"];
+        public Double regularMarketChange => Fields["regularMarketChange"];
+        public Double regularMarketOpen => Fields["regularMarketOpen"];
+        public Double regularMarketDayHigh => Fields["regularMarketDayHigh"];
+        public Double regularMarketDayLow => Fields["regularMarketDayLow"];
+        public Int64 regularMarketVolume => Fields["regularMarketVolume"];
+        public String exchangeTimezoneName => Fields["exchangeTimezoneName"];
+        public String shortName => Fields["shortName"];
+        public String exchange => Fields["exchange"];
+        public String market => Fields["market"];
+        public String marketState => Fields["marketState"];
+        public Int64 exchangeDataDelayedBy => Fields["exchangeDataDelayedBy"];
+        public String symbol => Fields["symbol"];
+    }
 }
